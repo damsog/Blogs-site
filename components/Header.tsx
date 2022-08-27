730c08426da178456246c66823991f4c4ec7af33
kaiser24 <damsog38@gmail.com>
2022-08-27 12:56:45 -0500

    Working on nav bar ui styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,15 +4,19 @@ import Link from "next/link";
 const Header = () => {
     return (
         <header>
-            <div>
+            <div className="flex items-center space-x-5">
                 <Link href="/">
                     <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" 
                     alt="" />
                 </Link>
+                <div className="hidden md:inline-flex 
+                    items-center space-x-5">
+                    <h3>About</h3>
+                    <h3>Contact</h3>
+                    <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
+                </div>
             </div>
-            <div>
-
-            </div>
+            <div></div>
         </header>
     );
 }
