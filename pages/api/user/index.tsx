3b5125d1126e2477b2a01fd7565596d5ebee3b74
felipe.serna <damsog38@gmail.com>
2022-08-31 15:07:54 -0500

    Modified the user controller to handle both get and post methods

--- pages/api/user/index.tsx
+++ pages/api/user/index.tsx
@@ -1,11 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import userService, { UserToCreate } from "../../../services/userService";
 
 const prisma = new PrismaClient();
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const users = await prisma.user.findMany({});
-    res.status(200).json(users);
+interface UserCreateRequest extends NextApiRequest {
+    body: UserToCreate
+}
+
+
+const handler = async (req: UserCreateRequest, res: NextApiResponse) => {
+    const { body } = req;
+    switch(req.method){
+        case "GET":{
+            const users = await userService.findAll();
+            res.status(200).json(users);
+            break;
+        }
+        case "POST":{
+            if(
+                'username' in body && typeof body.username === 'string' &&
+                'password' in body && typeof body.password === 'string' &&
+                'email' in body && typeof body.email === 'string'
+            ){
+                const user = await userService.create(body);
+     
+                return res.status(200).json(user);
+            }else{
+                return res.status(400).json({message: "Invalid request"});
+            }
+        }
+        default:{
+            return res.status(400).json({message:"Invalid request"});
+        }
+    }
 }
 
 export default handler;
