3ada7ccb02418e5683cbdd281d5a3bb06c6e4de5
felipe.serna <damsog38@gmail.com>
2022-09-02 10:56:51 -0500

    Get posts by user id endpoint

---
+++ pages/api/post/byuser/[id].tsx
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import postService, { PostDataModel } from "../../../../services/postService";
+
+interface PostRequest extends NextApiRequest {
+    query:
+    {id: string;}
+}
+
+const handler = async (req: PostRequest, res: NextApiResponse) => {
+    const { id } = req.query;
+    switch(req.method){
+        case "GET":{
+            const post = await postService.findByAuthorId(id);
+            return res.status(200).json(post);
+        }
+        default:{
+            return res.status(400).json({message:"Invalid request"});
+        }
+    }
+}
+
+export default handler;
