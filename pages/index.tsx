34b47ad5b25cac6a8bf205474535f340ef3165a8
kaiser24 <damsog38@gmail.com>
2022-09-04 11:55:46 -0500

    Requesting all posts with SSR for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import logger from '../lib/logger';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { Post } from '@prisma/client';
 
-const Home: NextPage = () => {
+interface Props {
+  posts: Post[] | undefined;
+}
+
+const Home: NextPage<Props> = ({posts}:Props) => {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -36,5 +44,27 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  dotenv.config();
+  const api = process.env.API_URL;
+  let posts:Post|undefined;
+
+  logger.debug(api);
+  try{
+    const res = await axios.get(`${api}/post`);
+    posts = res.data;
+  }catch(err){
+    logger.error(err);
+    posts = undefined;
+  }
+
+  return {
+    props: {
+      posts,
+    }
+  } 
+}
+
 
 export default Home
