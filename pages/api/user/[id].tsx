57afeacf612cc43ae6bf6b45c760fd6172007c1b
kaiser24 <damsog38@gmail.com>
2022-09-04 19:48:18 -0500

    Changed basic next api to next-connect for user controller

--- pages/api/user/[id].tsx
+++ pages/api/user/[id].tsx
@@ -1,35 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import userService from "../../../services/userService";
+import { createRouter, expressWrapper } from "next-connect";
+import customMorgan from "../../../lib/customMorgan";
 
 interface UserRequest extends NextApiRequest {
     query:
     {id: string;}
 }
 
-const handler = async (req: UserRequest, res: NextApiResponse) => {
-    const { body } = req;
-    const { id } = req.query;
-    switch(req.method){
-        case "GET":{
-            const user = await userService.findById(id);
-            return res.status(200).json(user);
-        }
-        case "PUT":{
-            const user = await userService.update(id, body);
-            return res.status(200).json(user);
-        }
-        case "DELETE":{
-            try{
-                const user = await userService.delete(id);
-                return res.status(200).json(user);
-            }catch(e){
-                return res.status(400).json({message: "Record to delete does not exist."});
-            }
-        }
-        default:{
-            return res.status(400).json({message:"Invalid request"});
-        }
-    }
-}
+const router = createRouter<UserRequest, NextApiResponse>();
 
-export default handler;
+router.use(expressWrapper(customMorgan));
+
+router.get( async (req: UserRequest, res: NextApiResponse) =>{
+    const user = await userService.findById(req.query.id);
+    return res.status(200).json(user);
+});
+
+router.put( async (req: UserRequest, res: NextApiResponse) =>{
+    const user = await userService.update(req.query.id, req.body);
+    return res.status(200).json(user);
+});
+
+router.delete( async (req: UserRequest, res: NextApiResponse) =>{
+    try{
+        const user = await userService.delete(req.query.id);
+        return res.status(200).json(user);
+    }catch(e){
+        return res.status(400).json({message: "Record to delete does not exist."});
+    }
+});
+
+export default router.handler({
+    onNoMatch(req: UserRequest, res: NextApiResponse){
+        res.status(405).json({message: `Method ${req.method} Not Allowed`});
+    }
+});
--- pages/api/user/index.tsx
+++ pages/api/user/index.tsx
@@ -1,37 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import userService, { UserDataModel } from "../../../services/userService";
+import { createRouter, expressWrapper } from "next-connect";
+import customMorgan from "../../../lib/customMorgan";
 
-interface UserCreateRequest extends NextApiRequest {
+interface UserRequest extends NextApiRequest {
     body: UserDataModel
 }
 
+const router = createRouter<UserRequest, NextApiResponse>();
 
-const handler = async (req: UserCreateRequest, res: NextApiResponse) => {
+router.use(expressWrapper(customMorgan));
+
+router.get( async (req: UserRequest, res: NextApiResponse) =>{
+    const users = await userService.findAll();
+    return res.status(200).json(users);
+});
+
+router.post( async (req: UserRequest, res: NextApiResponse) =>{
     const { body } = req;
-    switch(req.method){
-        case "GET":{
-            const users = await userService.findAll();
-            return res.status(200).json(users);
+    try{
+        if(
+            "username" in body && typeof body.username === "string" &&
+            "password" in body && typeof body.password === "string" &&
+            "email" in body && typeof body.email === "string"
+        ){
+            const user = await userService.create(body);
+            return res.status(200).json(user);
         }
-        case "POST":{
-            try{
-                if(
-                    "username" in body && typeof body.username === "string" &&
-                    "password" in body && typeof body.password === "string" &&
-                    "email" in body && typeof body.email === "string"
-                ){
-                    const user = await userService.create(body);
-                    return res.status(200).json(user);
-                }
-                return res.status(400).json({message: "Invalid request"});
-            }catch(e){
-                return res.status(400).json({message: `Error creating User ${e}`});
-            }
-        }
-        default:{
-            return res.status(400).json({message:"Invalid request"});
-        }
+        return res.status(400).json({message: "Invalid request"});
+    }catch(e){
+        return res.status(400).json({message: `Error creating User ${e}`});
     }
-}
+});
 
-export default handler;
+export default router.handler({
+    onNoMatch(req: UserRequest, res: NextApiResponse){
+        res.status(405).json({message: `Method ${req.method} Not Allowed`});
+    },
+});
