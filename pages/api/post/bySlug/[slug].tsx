f5b7b6a27e87b0a62a83a1f13f6df96aa8b35149
kaiser24 <damsog38@gmail.com>
2022-09-10 17:40:38 -0500

    Create endpoint to query post by their slug

---
+++ pages/api/post/bySlug/[slug].tsx
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import postService from "../../../../services/postService";
+import { createRouter, expressWrapper } from "next-connect";
+import customMorgan from "../../../../lib/customMorgan";
+
+interface PostRequest extends NextApiRequest {
+    query:
+    {slug: string;}
+}
+
+const router = createRouter<PostRequest, NextApiResponse>();
+
+router.use(expressWrapper(customMorgan));
+
+router.get( async (req: PostRequest, res: NextApiResponse) =>{
+    const post = await postService.findBySlug(req.query.slug);
+    return res.status(200).json(post);
+});
+
+export default router.handler({
+    onNoMatch(req: PostRequest, res: NextApiResponse){
+        res.status(405).json({message: `Method ${req.method} Not Allowed`});
+    }
+});
