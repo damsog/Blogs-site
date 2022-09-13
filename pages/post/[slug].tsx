e0d0b6948cbe2a6c73268faa4687296b2bae8c4d
kaiser24 <damsog38@gmail.com>
2022-09-12 20:54:24 -0500

    Created a form to leave a comment below a post. also styled it

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -49,6 +49,41 @@ function Post({post}: Props) {
                     <ReactMarkdown>{post.content}</ReactMarkdown>
                 </div>
             </article>
+
+            <hr className="max-w-lg my-5 mx-auto border border-yellow-500"/>
+
+            <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+                <h3 className="text-5m text-yellow-500 ">Enjoyed this article?</h3>
+                <h4 className="text-3xl font-bold">Leave a Comment below!</h4>
+                <hr className="py-3 mt-2"/>
+                <label className="block mb-5">
+                    <span className="text-gray-700">Name</span>
+                    <input
+                     className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500
+                     outline-none focus:ring" 
+                     placeholder="John Doe" 
+                     type="text" 
+                    />
+                </label>
+                <label className="block mb-5">
+                    <span className="text-gray-700">Email</span>
+                    <input
+                     className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500
+                     outline-none focus:ring" 
+                     placeholder="John Doe" 
+                     type="text" 
+                    />
+                </label>
+                <label className="block mb-5">
+                    <span className="text-gray-700">Comment</span>
+                    <textarea
+                     className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500
+                     outline-none focus:ring" 
+                     placeholder="John Doe" 
+                     rows={8} 
+                    />
+                </label>
+            </form>
         </main>
     );
 };
