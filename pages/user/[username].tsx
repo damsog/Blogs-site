3e292a16426466943fba67286b13a7178d15ab83
kaiser24 <damsog38@gmail.com>
2022-09-24 21:45:32 -0500

    Basic user personal view page

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -3,6 +3,10 @@ import { GetStaticPaths, GetStaticProps } from "next/types";
 import Header from "../../components/Header";
 import userService from "../../services/userService";
 import { User } from "@prisma/client";
+import Stories from "../../components/Stories";
+import { useState } from "react";
+import Account from "../../components/Account";
+import Write from "../../components/Write";
 
 
 interface PropsI {
@@ -10,31 +14,43 @@ interface PropsI {
 }
 
 function user({user}: PropsI) {
+    const [ selection, setSelection ] = useState("stories");
     const { data: session, status } = useSession();
 
     return (
         <main>
             {session ? (
-                <div className="relative grid grid-cols-4 grid-rows-5 gap-5 h-screen w-auto bg-blue-100">
-                    <div className="row-span-2 col-span-1 bg-blue-200"></div>
-                    <div className="col-span-3 row-span-5 bg-blue-200">
-                        <div className="">Aomething</div>    
+                <div className=" grid grid-cols-7 grid-rows-7 divide-x h-screen w-auto">
+                    <div className="row-span-4 col-span-1 "></div>
+                    <div className="relative col-span-6 row-span-6">
+                        {(selection === "stories") && (<Stories/>)}
+
+                        {(selection === "write") && (<Write/>)}
+
+                        {(selection === "account") && (<Account/>)}
                     </div>
-                    <div className="relative bg-blue-200">
-                        <div className="absolute bottom-0 right-0">
-                            <p>Following</p>
+                    <div className="relative row-span-1 col-span-1 ">
+                        <div className="m-4 absolute bottom-1/2 right-0">
+                            {(selection === "stories") ? 
+                                (<h5 className="cursor-pointer text-gray-900" onClick={()=>setSelection("stories")}>Stories</h5>):
+                                (<h5 className="cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("stories")}>Stories</h5>)}
                         </div>
                     </div>
-                    <div className="relative bg-blue-200">
-                        <div className="absolute top-0 right-0">
-                            <p>Edit</p> 
+                    <div className="relative row-span-1 col-span-1 ">
+                        <div className="m-4 absolute bottom-1/2 right-0">
+                            {(selection === "write") ? 
+                                (<h5 className="cursor-pointer text-gray-900" onClick={()=>setSelection("write")}>Write</h5>):
+                                (<h5 className="cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("write")}>Write</h5>)}
                         </div>
                     </div>
-                    <div className="relative bg-blue-200">
-                        <div className="absolute top-0 right-0">
-                            Account
+                    <div className="relative row-span-1 col-span-1 ">
+                        <div className="m-4 absolute top-1/2 right-0">
+                            {(selection === "account") ? 
+                                (<h5 className="cursor-pointer text-gray-900" onClick={()=>setSelection("account")}>Account</h5>):
+                                (<h5 className="cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("account")}>Account</h5>)}
                         </div>
                     </div>
+                    
                 </div>
             ) : (
                 <p>Not logged</p>
