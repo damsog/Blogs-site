03d03b2bf665cbdea20f00fca8b2342b1c587d3d
kaiser24 <damsog38@gmail.com>
2022-09-24 21:45:02 -0500

    Working on the Write component

---
+++ components/Write.tsx
@@ -0,0 +1,11 @@
+const Write = () => {
+    return (
+        <div className="h-full w-full">
+            <div className="flex flex-col items-center p-10">
+                <h1 className=" text-4xl font-bold ">Write</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Write;
