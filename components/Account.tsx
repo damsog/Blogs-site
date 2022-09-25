881d8cd5de0c6bd4dc724e6ff96ded4828c2b216
kaiser24 <damsog38@gmail.com>
2022-09-24 21:44:43 -0500

    Working on the Account component

---
+++ components/Account.tsx
@@ -0,0 +1,11 @@
+const Account = () => {
+    return (
+        <div className="h-full w-full">
+            <div className="flex flex-col items-center p-10">
+                <h5 className=" text-4xl font-bold ">Account</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Account;
