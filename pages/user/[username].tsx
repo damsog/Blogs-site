74cff17e2e5a92242ec42c50ac2f8d929d6b818d
kaiser24 <damsog38@gmail.com>
2022-09-27 03:15:11 -0500

    Added react icons

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -7,6 +7,8 @@ import Stories from "../../components/Stories";
 import { useState } from "react";
 import Account from "../../components/Account";
 import Write from "../../components/Write";
+import { IoBookmarksOutline } from "@react-icons/all-files/io5/IoBookmarksOutline";
+import { IoBookOutline } from "@react-icons/all-files/io5/IoBookOutline";
 
 
 interface PropsI {
@@ -32,22 +34,22 @@ function user({user}: PropsI) {
                     <div className="relative row-span-1 col-span-1 ">
                         <div className="m-4 absolute bottom-1/2 right-0">
                             {(selection === "stories") ? 
-                                (<h5 className="cursor-pointer text-gray-900" onClick={()=>setSelection("stories")}>Stories</h5>):
-                                (<h5 className="cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("stories")}>Stories</h5>)}
+                                (<h5 className=" text-3xl cursor-pointer text-gray-900" onClick={()=>setSelection("stories")}><IoBookmarksOutline/></h5>):
+                                (<h5 className=" text-3xl cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("stories")}><IoBookmarksOutline/></h5>)}
                         </div>
                     </div>
                     <div className="relative row-span-1 col-span-1 ">
                         <div className="m-4 absolute bottom-1/2 right-0">
                             {(selection === "write") ? 
-                                (<h5 className="cursor-pointer text-gray-900" onClick={()=>setSelection("write")}>Write</h5>):
-                                (<h5 className="cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("write")}>Write</h5>)}
+                                (<h5 className=" text-3xl cursor-pointer text-gray-900" onClick={()=>setSelection("write")}><IoBookOutline/></h5>):
+                                (<h5 className=" text-3xl cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("write")}><IoBookOutline/></h5>)}
                         </div>
                     </div>
                     <div className="relative row-span-1 col-span-1 ">
-                        <div className="m-4 absolute top-1/2 right-0">
+                        <div className="m-4 absolute top-0 right-0">
                             {(selection === "account") ? 
-                                (<h5 className="cursor-pointer text-gray-900" onClick={()=>setSelection("account")}>Account</h5>):
-                                (<h5 className="cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("account")}>Account</h5>)}
+                                (<img className="h-10 w-10 rounded-full cursor-pointer outline-double outline-black-900" onClick={()=>setSelection("account")} src={user.image!} alt=""/>):
+                                (<img className="h-10 w-10 rounded-full cursor-pointer hover hover:outline-double hover:outline-black-900" onClick={()=>setSelection("account")} src={user.image!} alt=""/>)}
                         </div>
                     </div>
                     
