43dc242322ac12f05294b919f1ac88809ad917c2
kaiser24 <damsog38@gmail.com>
2022-09-29 23:10:48 -0500

    Added sidebar to user page

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -11,6 +11,7 @@ import { IoBookmarksOutline } from "@react-icons/all-files/io5/IoBookmarksOutlin
 import { IoBookOutline } from "@react-icons/all-files/io5/IoBookOutline";
 import postService from "../services/postService";
 import Link from "next/link";
+import SideBar from "../components/SideBar";
 
 
 interface PropsI {
@@ -26,33 +27,9 @@ function user({user, posts}: PropsI) {
         <main>
             {session ? (
                 <div className="flex flex-row justify-between h-screen w-auto">
-                    <div className="flex flex-col justify-between w-1/6">
-                        <div className="relative">
-                            <Link key="/" href="/">
-                                <img className="absolute top-0 right-0 w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="" />
-                            </Link>
-                        </div>
-                        <div className="relative">
-                            <div className="absolute m-4 top-0 right-0">
-                                {(selection === "stories") ? 
-                                    (<h5 className=" text-2xl cursor-pointer text-gray-900" onClick={()=>setSelection("stories")}><IoBookmarksOutline/></h5>):
-                                    (<h5 className=" text-2xl cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("stories")}><IoBookmarksOutline/></h5>)}
-                            </div>
-
-                            <div className="absolute m-4 bottom-0 right-0">
-                                {(selection === "write") ? 
-                                    (<h5 className=" text-2xl cursor-pointer text-gray-900" onClick={()=>setSelection("write")}><IoBookOutline/></h5>):
-                                    (<h5 className=" text-2xl cursor-pointer text-gray-500 hover hover:text-gray-900" onClick={()=>setSelection("write")}><IoBookOutline/></h5>)}
-                            </div>
-                        </div>
-                        <div className="relative">
-                            <div className="absolute m-4 bottom-0 right-0 ">
-                                {(selection === "account") ? 
-                                    (<img className="h-8 w-8 rounded-full cursor-pointer outline-double outline-black-900" onClick={()=>setSelection("account")} src={user.image!} alt=""/>):
-                                    (<img className="h-8 w-8 rounded-full cursor-pointer hover hover:outline-double hover:outline-black-900" onClick={()=>setSelection("account")} src={user.image!} alt=""/>)}
-                            </div>
-                        </div>
-                    </div>
+                    
+                    <SideBar className="w-1/6" user={user} selection={selection} setSelectionEvent={setSelection}></SideBar>  
+                    
                     <div className=" w-8/12 ">
                         <div className=" px-24 ">
                             {(selection === "stories") && (<Stories posts={posts} user={user} />)}
